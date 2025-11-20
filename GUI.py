563a0353aba7dcaa330ab9ff138b5fae76d5a5c5
Uricoh <uricohen444@gmail.com>
2025-11-20 21:23:17 +0200

    Login system finished (hopefully)

--- GUI.py
+++ GUI.py
@@ -7,20 +7,16 @@ import protocol
 class GUI:
     def __init__(self):
         self._root = tk.Tk()
-        self._bg_pimage: str = ""
-        self.create_ui()
-        self._started: bool = False
-
-    def create_ui(self):
         self._root.title("Currency Converter - Start Page")
         self._root.geometry(f"{protocol.size1}x{protocol.size2}")
 
         # Show background image
         bg_image = Image.open(protocol.bg_path)
         bg_reimage = bg_image.resize((protocol.size1, protocol.size2))
-        self._bg_pimage: PhotoImage = ImageTk.PhotoImage(bg_reimage)
-        bg_label = tk.Label(self._root, image=self._bg_pimage)
-        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.bg_pimage: PhotoImage = ImageTk.PhotoImage(bg_reimage)
+        self.bg_label = tk.Label(self._root, image=self.bg_pimage)
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self._started: bool = False
 
     def run(self):
         self._root.mainloop()
