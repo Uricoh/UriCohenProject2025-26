b2e49e0208ff767f7a178765aedc7a5759daa8e7
Uricoh <uricohen444@gmail.com>
2025-12-01 09:27:45 +0200

    Revamped ClientGUI, minor changes to other files

--- ClientBL.py
+++ ClientBL.py
@@ -4,26 +4,22 @@ import socket
 
 class ClientBL:
     def __init__(self):
-        self._socket: socket = None
+        self.socket: socket = None
 
     def on_click_start(self):
         # Login start button
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         protocol.logger.info("[CLIENTBL] - Client socket created")
         protocol.logger.info("[CLIENTBL] - Start button clicked")
-        self._socket.connect((protocol.SERVER_IP, protocol.PORT))
+        self.socket.connect((protocol.SERVER_IP, protocol.PORT))
         protocol.logger.info("[CLIENTBL] - Client connected to server")
 
     def on_click_stop(self):
         # Both login and main stop buttons
         protocol.logger.info("[CLIENTBL] - Stop button clicked")
-        self._socket.close()
+        self.socket.close()
         protocol.logger.info("[CLIENTBL] - Client socket closed")
 
     def send_data(self, data):
-        self._socket.sendall(data.encode('utf-8'))
+        self.socket.sendall(data.encode('utf-8'))
         protocol.logger.info("[CLIENTBL] - Data sent to server")
-
-    def get_socket(self):
-        # Exists because _socket is protected
-        return self._socket
